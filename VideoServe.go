30a3dc68449377371a6de2e04881a8cc082e7005
ThyLeader <colin.adler@yahoo.com>
2015-10-11 22:08:57 -0400

    Should work

--- VideoServe.go
+++ VideoServe.go
@@ -8,10 +8,52 @@ import (
 )
 
 var VideoList = []string{
+  "https://www.youtube.com/watch?v=tE1HDSipRxU",
+  "http://img.4plebs.org/boards/f/image/1434/92/1434921182936.swf",
+  "http://img.4plebs.org/boards/f/image/1442/53/1442530025364.swf",
+  "http://img.4plebs.org/boards/f/image/1396/24/1396243585761.swf",
+  "http://img.4plebs.org/boards/f/image/1421/12/1421126458648.swf",
+  "https://www.youtube.com/watch?v=G2e_M06YDyY",
+  "https://www.youtube.com/watch?v=DZGINaRUEkU",
+  "https://www.youtube.com/watch?v=otnyM9RJG4o",
+  "https://www.youtube.com/watch?v=2MN1vXO5JeI",
+  "https://www.youtube.com/watch?v=u_jRgv-UqBU",
+  "https://www.youtube.com/watch?v=DqC7H7_Noi8",
+  "https://www.youtube.com/watch?v=2rEuie5lpGA",
+  "http://i.4cdn.org/wsg/1444358103893.webm",
+  "http://i.4cdn.org/wsg/1443246935183.webm",
+  "http://i.4cdn.org/wsg/1443251102863.webm",
+  "http://i.4cdn.org/wsg/1443254714327.webm",
+  "http://i.4cdn.org/wsg/1443913579648.webm",
+  "http://i.4cdn.org/wsg/1444112721405.webm",
+  "http://i.4cdn.org/wsg/1443246656431.webm",
+  "http://i.4cdn.org/wsg/1443199345935.webm",
+  "http://i.4cdn.org/wsg/1443199345935.webm",
+  "http://i.4cdn.org/wsg/1443199345935.webm",
+  "http://i.4cdn.org/wsg/1443199345935.webm",
+  "http://i.4cdn.org/wsg/1443199345935.webm",
+  "http://i.4cdn.org/wsg/1443199345935.webm",
+  "http://i.4cdn.org/wsg/1443199345935.webm",
+  "http://i.4cdn.org/wsg/1443199345935.webm",
+  "http://i.4cdn.org/wsg/1443199345935.webm",
+  "http://i.4cdn.org/wsg/1443199345935.webm",
+  "http://i.4cdn.org/wsg/1443199345935.webm",
+  "http://i.4cdn.org/wsg/1443199345935.webm",
+  "http://i.4cdn.org/wsg/1443199345935.webm",
+  "http://i.4cdn.org/wsg/1443199345935.webm"
+}
 
+func RandLink() {
+  r := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
+  vid := r.Intn(len(VideoList[])) + 1;
+  return VideoList[vid]
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+  newVideo := RandLink()
+  http.Redirect(w, r, newVideo, 301)
 }
 
 func init() {
     http.HandleFunc("/", Index)
-    http.HandleFunc("/.*", Index)
 }
