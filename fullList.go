58f736a85ecb52e9d86324c308a92f08c8db5127
ThyLeader <colin.adler@yahoo.com>
2015-10-11 21:51:03 -0400

    Many more videos

--- fullList.go
+++ fullList.go
@@ -12,5 +12,26 @@ https://www.youtube.com/watch?v=2MN1vXO5JeI                           WTF Singal
 https://www.youtube.com/watch?v=u_jRgv-UqBU                           Walmart Greeter         //
 https://www.youtube.com/watch?v=DqC7H7_Noi8                           Neil Armstrong          //
 https://www.youtube.com/watch?v=2rEuie5lpGA                           SURF THE NET            //
+http://i.4cdn.org/wsg/1444358103893.webm                              Thug Cat
+http://i.4cdn.org/wsg/1443246935183.webm                              Plane Crash
+http://i.4cdn.org/wsg/1443251102863.webm                              Old Spice 16 hrs
+http://i.4cdn.org/wsg/1443254714327.webm                              McDonald's Remix
+http://i.4cdn.org/wsg/1443913579648.webm                              Sledgehammers
+http://i.4cdn.org/wsg/1444112721405.webm                              Frozen Jesse Pinkman
+http://i.4cdn.org/wsg/1443246656431.webm                              Talking Carl
+http://i.4cdn.org/wsg/1443199345935.webm                              Mario Head Bang
+http://i.4cdn.org/wsg/1443199345935.webm                              Gin+Juice News
+http://i.4cdn.org/wsg/1443199345935.webm                              Tomato -> fan
+http://i.4cdn.org/wsg/1443199345935.webm                              Old Rollerblader
+http://i.4cdn.org/wsg/1443199345935.webm                              Rugby Kid
+http://i.4cdn.org/wsg/1443199345935.webm                              Science is Beautiful
+http://i.4cdn.org/wsg/1443199345935.webm                              Hitler Leek
+http://i.4cdn.org/wsg/1443199345935.webm                              Bobcat Load
+http://i.4cdn.org/wsg/1443199345935.webm                              Throwing Knives
+http://i.4cdn.org/wsg/1443199345935.webm                              Circle
+http://i.4cdn.org/wsg/1443199345935.webm                              Yodeling
+http://i.4cdn.org/wsg/1443199345935.webm                              Leek + Gun
+http://i.4cdn.org/wsg/1443199345935.webm                              Cowbell
+http://i.4cdn.org/wsg/1443199345935.webm                              Arnold Palmer
 
 */
