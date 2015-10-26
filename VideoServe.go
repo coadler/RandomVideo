5b1497879619a1d7f680dd515590f7c4b782e77b
ThyLeader <colin@cadler.co>
2015-10-26 09:55:20 -0400

    Fixed broken 4chan links

--- VideoServe.go
+++ VideoServe.go
@@ -74,48 +74,48 @@ func RandLink() (string, int){
     "https://www.youtube.com/watch?v=u_jRgv-UqBU",                          // 9  Billy, Walmart Greeter
     "https://www.youtube.com/watch?v=DqC7H7_Noi8",                          // 10  Neil Armstrong Tribute
     "https://www.youtube.com/watch?v=2rEuie5lpGA",                          // 11 SURF THE NET
-    "http://i.4cdn.org/wsg/1444358103893.webm",                             // 12 Thug Cat
-    "http://i.4cdn.org/wsg/1443246935183.webm",                             // 13 Plane Crash
-    "http://i.4cdn.org/wsg/1443251102863.webm",                             // 14 Old Spice
-    "http://i.4cdn.org/wsg/1443254714327.webm",                             // 15 McDonald's Remix
-    "http://i.4cdn.org/wsg/1443913579648.webm",                             // 16 Sledge Hammer
-    "http://i.4cdn.org/wsg/1444112721405.webm",                             // 17 Frozen Jesse Pinkman
+    "http://data.desustorage.org/4ch/wsg/image/1443/69/1443692537150.webm", // 12 Thug Cat
+    "http://data.desustorage.org/4ch/wsg/image/1443/24/1443246935183.webm", // 13 Plane Crash
+    "http://data.desustorage.org/4ch/wsg/image/1443/25/1443251102863.webm", // 14 Old Spice
+    "http://data.desustorage.org/4ch/wsg/image/1443/13/1443133300928.webm", // 15 McDonald's Remix
+    "http://data.desustorage.org/4ch/wsg/image/1443/91/1443913579648.webm", // 16 Sledge Hammer
+    "http://data.desustorage.org/4ch/wsg/image/1444/11/1444112721405.webm", // 17 Frozen Jesse Pinkman
     "http://data.desustorage.org/4ch/wsg/image/1441/07/1441076705814.webm", // 18 Talking Carl
-    "http://i.4cdn.org/wsg/1443199345935.webm",                             // 19 Mario Head Bang
-    "http://i.4cdn.org/wsg/1443246507978.webm",                             // 20 Gin+Juice
-    "http://i.4cdn.org/wsg/1444262136311.webm",                             // 21 Tomato -> Fan
-    "http://i.4cdn.org/wsg/1444262646541.webm",                             // 22 Old Man + Rollerskates
-    "http://i.4cdn.org/wsg/1444268978629.webm",                             // 23 Rugby Kid
-    "http://i.4cdn.org/wsg/1444276631906.webm",                             // 24 Beautiful Science
-    "http://i.4cdn.org/wsg/1444301298445.webm",                             // 25 Hitler Leek
-    "http://i.4cdn.org/wsg/1444314579514.webm",                             // 26 Bobcat Loading
-    "http://i.4cdn.org/wsg/1444339262566.webm",                             // 27 Throwing Knives
-    "http://i.4cdn.org/wsg/1444339554930.gif",                              // 28 Perfect Circle
-    "http://i.4cdn.org/wsg/1444372368654.webm",                             // 29 Yodeling
-    "http://i.4cdn.org/wsg/1444415934593.webm",                             // 30 Leek Gun
-    "http://i.4cdn.org/wsg/1444448380633.webm",                             // 31 Cow Bell
-    "http://i.4cdn.org/wsg/1444467926220.webm",                             // 32 Arnold Palmer
+    "http://data.desustorage.org/4ch/wsg/image/1443/10/1443109512298.webm", // 19 Mario Head Bang
+    "http://data.desustorage.org/4ch/wsg/image/1443/03/1443034574003.webm", // 20 Gin+Juice
+    "http://data.desustorage.org/4ch/wsg/image/1444/26/1444262136311.webm", // 21 Tomato -> Fan
+    "http://data.desustorage.org/4ch/wsg/image/1444/26/1444262646541.webm", // 22 Old Man + Rollerskates
+    "http://data.desustorage.org/4ch/wsg/image/1444/26/1444268978629.webm", // 23 Rugby Kid
+    "http://data.desustorage.org/4ch/wsg/image/1440/94/1440943905455.webm", // 24 Beautiful Science
+    "http://data.desustorage.org/4ch/wsg/image/1442/85/1442851024053.webm", // 25 Hitler Leek
+    "http://data.desustorage.org/4ch/wsg/image/1444/31/1444314579514.webm", // 26 Bobcat Loading
+    "http://data.desustorage.org/4ch/wsg/image/1444/33/1444339262566.webm", // 27 Throwing Knives
+    "http://data.desustorage.org/4ch/wsg/image/1444/33/1444339554930.gif",  // 28 Perfect Circle
+    "http://data.desustorage.org/4ch/wsg/image/1444/37/1444372368654.webm", // 29 Yodeling
+    "http://data.desustorage.org/4ch/wsg/image/1444/41/1444415934593.webm", // 30 Leek Gun
+    "http://data.desustorage.org/4ch/wsg/image/1444/22/1444228404320.webm", // 31 Cow Bell
+    "http://data.desustorage.org/4ch/wsg/image/1444/46/1444467926220.webm", // 32 Arnold Palmer
     "https://www.youtube.com/watch?v=2svVkkNuSq0",                          // 33 Stop to my Beat
     "https://www.youtube.com/watch?v=DX_eeOZVS2o",                          // 34 Microwave Dance
-    "http://i.4cdn.org/wsg/1443899505305.webm",                             // 35 I Don't Need a Jacket
-    "http://i.4cdn.org/wsg/1444055325919.webm",                             // 36 GTFO
-    "http://i.4cdn.org/wsg/1441602608583.webm",                             // 37 Steven Universe
-    "http://i.4cdn.org/wsg/1444244530145.webm",                             // 38 BAD BOYZ
-    "http://i.4cdn.org/wsg/1444254081149.webm",                             // 39 BEER ME
-    "http://i.4cdn.org/wsg/1444371392998.webm",                             // 40 Pro Dad
-    "http://i.4cdn.org/wsg/1444440546084.webm",                             // 41 Drum Dog
-    "http://i.4cdn.org/wsg/1440917752642.webm",                             // 42 Get Down Cat
-    "http://i.4cdn.org/wsg/1440917996496.webm",                             // 43 Chicken Pokemon
-    "http://i.4cdn.org/wsg/1442157378169.webm",                             // 44 Dubstep Dog
-    "http://i.4cdn.org/wsg/1442157620314.webm",                             // 45 Shovel + Head
-    "http://i.4cdn.org/wsg/1442218215289.webm",                             // 46 Sly Kid
-    "http://i.4cdn.org/wsg/1442384146512.webm",                             // 47 Arriba
+    "http://data.desustorage.org/4ch/wsg/image/1444/17/1444175733369.webm", // 35 I Don't Need a Jacket
+    "http://data.desustorage.org/4ch/wsg/image/1444/05/1444055325919.webm", // 36 GTFO
+    "http://data.desustorage.org/4ch/wsg/image/1441/60/1441602608583.webm", // 37 Steven Universe
+    "http://data.desustorage.org/4ch/wsg/image/1444/24/1444244530145.webm", // 38 BAD BOYZ
+    "http://data.desustorage.org/4ch/wsg/image/1444/25/1444254081149.webm", // 39 BEER ME
+    "http://data.desustorage.org/4ch/wsg/image/1444/37/1444371392998.webm", // 40 Pro Dad
+    "http://data.desustorage.org/4ch/wsg/image/1442/75/1442751902230.webm", // 41 Drum Dog
+    "http://data.desustorage.org/4ch/wsg/image/1440/91/1440917752642.webm", // 42 Get Down Cat
+    "http://data.desustorage.org/4ch/wsg/image/1440/91/1440917996496.webm", // 43 Chicken Pokemon
+    "http://data.desustorage.org/4ch/wsg/image/1443/81/1443810282577.webm", // 44 Dubstep Dog
+    "http://data.desustorage.org/4ch/wsg/image/1442/15/1442157620314.webm", // 45 Shovel + Head
+    "http://data.desustorage.org/4ch/wsg/image/1442/21/1442218215289.webm", // 46 Sly Kid
+    "http://data.desustorage.org/4ch/wsg/image/1443/84/1443844978601.webm", // 47 Arriba
     "http://data.desustorage.org/4ch/wsg/image/1443/31/1443312573923.webm", // 48 Hot Boy Dog
     "http://i.4cdn.org/wsg/1440991924939.webm",                             // 49 More Doge
     "http://i.4cdn.org/wsg/1443393958007.webm",                             // 50 Even More Doge
     "http://i.imgur.com/J7VGU2g.gifv",                                      // 51 Kittens + Puppies
     "http://i.imgur.com/ZuMSuvM.gifv",                                      // 52 Doge on Ledge
-    //"http://i.4cdn.org/wsg/1445033632154.webm",                             // 53 First Kiss/Life Insurance
+    //"http://i.4cdn.org/wsg/1445033632154.webm",                           // 53 First Kiss/Life Insurance
     "http://i.4cdn.org/wsg/1445043480702.webm",                             // 54 Dancing Birdz
     "http://i.4cdn.org/wsg/1445058535377.webm",                             // 55 Wendy's Commercial
     "http://i.4cdn.org/wsg/1445068822578.webm",                             // 56 Water Bottle Kick
